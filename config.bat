0813b6aab8502032262af386e3efce64ca1c865a
Kaoulombre <83710753+Kaoulombre@users.noreply.github.com>
2021-05-08 16:52:51 +0200

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,50 +1,67 @@
 @echo off
 
+:: TIME is in HOURS. If you don't specify a time for a coin, this time will be used
+set SWITCH_EVERY_HOUR=1
+:: Set your worker name here. You can enter any name you want, no space or symbols
+set WORKER_NAME=helium
+
 :: BEAM Coin (BEAM)
-set BEAM=CURRENTLY_UNAVAILABLE
+set BEAM=NO
 set BEAM_WALLET=
+set BEAM_MINING_TIME=
 :: BitcoinZ (BTCZ)
-set BTCZ=YES
+set BTCZ=NO
 set BTCZ_WALLET=t1ZQ68aM3q81gkUB9G6Fi5CXmujZQysyDEN
+set BTCZ_MINING_TIME=
 :: Bitzec (BZC)
-set BZC=CURRENTLY_UNAVAILABLE
+set BZC=NO
 set BZC_WALLET=t1W2JvfbE1ranBcPfDJ7jbuKjc4Ck8y3QyM
+set BZC_MINING_TIME=
 :: BZEdge (BZE)
 set BZE=NO
 set BZE_WALLET=t1ddWRt39Mnp3X12o35icVx3mWzVYjUHLAd
+set BZE_MINING_TIME=
 :: Zelcash (FLUX)
 set FLUX=NO
 set FLUX_WALLET=t1ZQ68aM3q81gkUB9G6Fi5CXmujZQysyDEN
+set FLUX_MINING_TIME=
 :: FOXDCoin (FOXD)
-set FOXD=CURRENTLY_UNAVAILABLE
+set FOXD=NO
 set FOXD_WALLET=F7uKVqX8GmJ8sdX3uxHQGPnkGxqHZ5mMu9
+set FOXD_MINING_TIME=
 :: LitecoinZ (LTZ)
-set LTZ=YES
+set LTZ=NO
 set LTZ_WALLET=L1GT3uYXeR49NCpxuFyX6LjutKmGXoVaXQD
+set LTZ_MINING_TIME=10
 :: RavenCoin (RVN)
-set RVN=NO
+set RVN=YES
 set RVN_WALLET=RSx44cGP1Ju2mccwJfE5CByADYHYqA2U8z
+set RVN_MINING_TIME=
 :: Space Coin (SPACE)
 set SPACE=NO
 set SPACE_WALLET=RRiiAaPUE5B4pqtUQehmBymfqazsWTwYu5
+set SPACE_MINING_TIME=
 :: Vidulum (VDL)
 set VDL=NO
 set VDL_WALLET=v1RCWRCSKmvoSNb9t4F2NMVR4t6CxxRAeE3
+set VDL_MINING_TIME=
 :: Snowgem TENT (XSG)
 set XSG=NO
 set XSG_WALLET=s1Ytqk64DSFsEPNNPWHZrgEmdiVqAcGsW5T
+set XSG_MINING_TIME=
 :: Ycash (YEC)
 set YEC=NO
 set YEC_WALLET=s1Ub5y7irLd61npqxDuNz4Xm6eFmTHqDkiz
+set YEC_MINING_TIME=
 :: Z Classic (ZCL)
-set ZCL=CURRENTLY_UNAVAILABLE
+set ZCL=NO
 set ZCL_WALLET=t1ZUYircJEBcZtMqjjwoJsBbRXaxAZkAQBp
+set ZCL_MINING_TIME=
 :: ZERO Coin (ZER)
 set ZER=NO
 set ZER_WALLET=t1ZcGDr4RL2KP3caD24gWJvwBRpy7iXEhRx
+set ZER_MINING_TIME=
 
-set SWITCH_EVERY_HOUR=1
-set WORKER_NAME=helium
 :: DONATIONS ARE OFF BY DEFAULT !! NO FEES
 :: DONATE=YES if you want to donate. Thanks!
 set DONATE=NO
