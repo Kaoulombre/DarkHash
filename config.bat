a179d8a67b6ae100bf8bcaaffb0a3922b77ef63e
Kaoulombre <83710753+Kaoulombre@users.noreply.github.com>
2021-05-08 14:42:55 +0200

    Add config file

---
+++ config.bat
@@ -0,0 +1,50 @@
+@echo off
+
+:: BEAM Coin (BEAM)
+set BEAM=NO
+set BEAM_WALLET=
+:: BitcoinZ (BTCZ)
+set BTCZ=YES
+set BTCZ_WALLET=t1ZQ68aM3q81gkUB9G6Fi5CXmujZQysyDEN
+:: Bitzec (BZC)
+set BZC=NO
+set BZC_WALLET=t1W2JvfbE1ranBcPfDJ7jbuKjc4Ck8y3QyM
+:: BZEdge (BZE)
+set BZE=NO
+set BZE_WALLET=t1ddWRt39Mnp3X12o35icVx3mWzVYjUHLAd
+:: Zelcash (FLUX)
+set FLUX=NO
+set FLUX_WALLET=t1ZQ68aM3q81gkUB9G6Fi5CXmujZQysyDEN
+:: FOXDCoin (FOXD)
+set FOXD=NO
+set FOXD_WALLET=F7uKVqX8GmJ8sdX3uxHQGPnkGxqHZ5mMu9
+:: LitecoinZ (LTZ)
+set LTZ=YES
+set LTZ_WALLET=L1GT3uYXeR49NCpxuFyX6LjutKmGXoVaXQD
+:: RavenCoin (RVN)
+set RVN=NO
+set RVN_WALLET=RSx44cGP1Ju2mccwJfE5CByADYHYqA2U8z
+:: Space Coin (SPACE)
+set SPACE=NO
+set SPACE_WALLET=RRiiAaPUE5B4pqtUQehmBymfqazsWTwYu5
+:: Vidulum (VDL)
+set VDL=NO
+set VDL_WALLET=v1RCWRCSKmvoSNb9t4F2NMVR4t6CxxRAeE3
+:: Snowgem TENT (XSG)
+set XSG=NO
+set XSG_WALLET=s1Ytqk64DSFsEPNNPWHZrgEmdiVqAcGsW5T
+:: Ycash (YEC)
+set YEC=NO
+set YEC_WALLET=s1Ub5y7irLd61npqxDuNz4Xm6eFmTHqDkiz
+:: Z Classic (ZCL)
+set ZCL=NO
+set ZCL_WALLET=t1ZUYircJEBcZtMqjjwoJsBbRXaxAZkAQBp
+:: ZERO Coin (ZER)
+set ZER=NO
+set ZER_WALLET=t1ZcGDr4RL2KP3caD24gWJvwBRpy7iXEhRx
+
+set SWITCH_EVERY_HOUR=1
+set WORKER_NAME=helium
+:: DONATIONS ARE OFF BY DEFAULT !! NO FEES
+:: DONATE=YES if you want to donate. Thanks!
+set DONATE=NO
